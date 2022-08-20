04935b7fd011a3d6a809ff66b6be91dc33b3298f
LaoShui <3048087505@qq.com>
2022-08-20 13:03:41 +0800

    增加启动音效+弹窗提示(仅Windows10以上版本)

--- src/main/java/net/ccbluex/liquidbounce/sound/TipSoundManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/sound/TipSoundManager.kt
@@ -1,4 +1,4 @@
-package net.ccbluex.liquidbounce.ui.sound
+package net.ccbluex.liquidbounce.sound
 
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.utils.FileUtils
@@ -7,10 +7,12 @@ import java.io.File
 class TipSoundManager {
     var enableSound: TipSoundPlayer
     var disableSound: TipSoundPlayer
+    var startup: TipSoundPlayer
 
     init {
         val enableSoundFile = File(LiquidBounce.fileManager.soundsDir, "enable.wav")
         val disableSoundFile = File(LiquidBounce.fileManager.soundsDir, "disable.wav")
+        val startupFile = File(LiquidBounce.fileManager.soundsDir, "startup.wav")
 
         if (!enableSoundFile.exists()) {
             FileUtils.unpackFile(enableSoundFile, "assets/minecraft/fdpclient/sound/enable.wav")
@@ -18,8 +20,12 @@ class TipSoundManager {
         if (!disableSoundFile.exists()) {
             FileUtils.unpackFile(disableSoundFile, "assets/minecraft/fdpclient/sound/disable.wav")
         }
+        if (!startupFile.exists()) {
+            FileUtils.unpackFile(startupFile, "assets/minecraft/fdpclient/sound/startup.wav")
+        }
 
         enableSound = TipSoundPlayer(enableSoundFile)
         disableSound = TipSoundPlayer(disableSoundFile)
+        startup = TipSoundPlayer(startupFile)
     }
 }
