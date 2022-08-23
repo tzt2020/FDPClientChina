fc9338fdf1eb37a13f0daa9aba802588541c17c1
XiGuaHanHan <63942405+xiguagege@users.noreply.github.com>
2022-08-24 01:51:13 +0800

    Add MatrixBhop & VeerisLowHop

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/matrix/MatrixBHop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/matrix/MatrixBHop.kt
@@ -4,60 +4,27 @@
  * https://github.com/UnlegitMinecraft/FDPClientChina/
  */
 package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.matrix
-
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
 import net.ccbluex.liquidbounce.utils.MovementUtils
-import net.ccbluex.liquidbounce.value.BoolValue
-import net.ccbluex.liquidbounce.value.FloatValue
 import net.minecraft.client.settings.GameSettings
-import kotlin.math.sqrt
 
-class MatrixBHop : SpeedMode("MatrixBHop") {
-    private val speedMultiValue = FloatValue("${valuePrefix}Speed", 1f, 0.7f, 1.2f)
-    private val noTimerValue = BoolValue("${valuePrefix}NoTimer", false)
-    private var recX = 0.0
-    private var recY = 0.0
-    private var recZ = 0.0
-    private var jumped = false
-    private var dist = 0.0
+class MatrixBHop : SpeedMode("MatrixHop2") {
+    private var ticks = 0
 
     override fun onUpdate() {
-        fun setTimer(value: Float) {
-            if(!noTimerValue.get()) {
-                mc.timer.timerSpeed = value
+        mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
+        if (MovementUtils.isMoving()) {
+            if (mc.thePlayer.onGround) {
+                mc.timer.timerSpeed = 1.0f
+                mc.thePlayer.jump()
             }
-        }
-        if (!mc.thePlayer.onGround) {
-            setTimer(1.0f)
-        }
-        if (jumped) {
-            mc.gameSettings.keyBindJump.pressed = GameSettings.isKeyDown(mc.gameSettings.keyBindJump)
-            jumped = false
-            setTimer(0.9f)
-            dist = sqrt((recX - mc.thePlayer.posX) * (recX - mc.thePlayer.posX) + (recZ - mc.thePlayer.posZ) * (recZ - mc.thePlayer.posZ))
-            if (MovementUtils.getSpeed() > 0) {
-                val recSpeed = MovementUtils.getSpeed()
-                mc.thePlayer.motionX *= (0.912 * dist * speedMultiValue.get()) / recSpeed
-                mc.thePlayer.motionZ *= (0.912 * dist * speedMultiValue.get()) / recSpeed
-            }
-        } else if (!mc.thePlayer.onGround) {
-            mc.thePlayer.jumpMovementFactor = 0.0265f
-        }
-        if (mc.thePlayer.onGround && MovementUtils.isMoving() && !mc.thePlayer.isInWater) {
-            recX = mc.thePlayer.posX
-            recY = mc.thePlayer.posY
-            recZ = mc.thePlayer.posZ
-            mc.gameSettings.keyBindJump.pressed = false
-            mc.thePlayer.jump()
-            MovementUtils.strafe()
-            jumped = true
-            setTimer(2.0f)
-        }
-    }
 
-    override fun onDisable() {
-        jumped = false
-        mc.timer.timerSpeed = 1f
-        mc.gameSettings.keyBindJump.pressed = mc.gameSettings.keyBindJump.isKeyDown
+            if (mc.thePlayer.motionY > 0.003) {
+                mc.thePlayer.motionX *= 1.0012
+                mc.thePlayer.motionZ *= 1.0012
+                mc.timer.timerSpeed = 1.05f
+            }
+        }
+
     }
 }
